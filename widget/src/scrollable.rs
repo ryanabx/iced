bbc281440d84657e6a313727146937cb47c01807
Austin M. Reppert <austinmreppert@protonmail.com>
2023-05-26 20:27:17 -0400

    Make vertical scroll properties optional
    Co-Authored-By: Austin M. Reppert <austinmreppert@gmail.com>

--- widget/src/scrollable.rs
+++ widget/src/scrollable.rs
@@ -102,7 +102,6 @@ where
         self.direction = direction;
         self
     }
-
     /// Sets a function to call when the [`Scrollable`] is scrolled.
     ///
     /// The function takes the [`Viewport`] of the [`Scrollable`]
