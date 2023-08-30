7e1d9a1a98c2281aae78e64c6e25a68a4908fcc7
Michael Aaron Murphy <michael@mmurphy.dev>
2023-08-30 16:28:51 +0200

    fix(widget): container inherited wrong icon color from renderer
    feat(renderer): define default icon color
    
    By default, this is the same as the text color for best visibility.
    
    fix(button): inherit icon color if set to none

--- core/src/renderer.rs
+++ core/src/renderer.rs
@@ -68,6 +68,8 @@ impl Default for Quad {
 /// The styling attributes of a [`Renderer`].
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Style {
+    /// The color to apply to symbolic icons.
+    pub icon_color: Color,
     /// The text color
     pub text_color: Color,
     /// The scale factor
@@ -77,6 +79,7 @@ pub struct Style {
 impl Default for Style {
     fn default() -> Self {
         Style {
+            icon_color: Color::BLACK,
             text_color: Color::BLACK,
             scale_factor: 1.0,
         }
