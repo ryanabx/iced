467ed8c67ba84a3b530264141bf5b221f55fdc2b
Ashley Wulber <ashley@system76.com>
2023-09-29 12:40:33 -0400

    fix: slider gradient angle

--- widget/src/slider.rs
+++ widget/src/slider.rs
@@ -17,7 +17,7 @@ use crate::core::{
 
 use std::ops::RangeInclusive;
 
-use iced_renderer::core::{border::Radius, Radians};
+use iced_renderer::core::{border::Radius, Degrees, Radians};
 pub use iced_style::slider::{
     Appearance, Handle, HandleShape, Rail, StyleSheet,
 };
@@ -677,7 +677,7 @@ pub fn draw<T, Theme, Renderer>(
                 ..renderer::Quad::default()
             },
             if auto_angle {
-                gradient.angle = Radians(0.0);
+                gradient.angle = Radians::from(Degrees(180.0));
                 gradient
             } else {
                 gradient
