d2cfea4d3923c5b20c4a3da88d941b78409a989c
Ashley Wulber <ashley@system76.com>
2023-12-07 18:44:18 -0500

    cleanup: formatting and clippy

--- examples/integration/src/main.rs
+++ examples/integration/src/main.rs
@@ -164,7 +164,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
 
     let mut state = program::State::new(
-        Id(0),
+        Id::MAIN,
         controls,
         viewport.logical_size(),
         &mut renderer,
