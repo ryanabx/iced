f49849aea482decb3b2c4f889c25358f126dbc1b
Ian Douglas Scott <idscott@system76.com>
2024-04-25 15:02:04 -0700

    sctk: more patches
    This adds an offset `Vector` as an argument to `on_drag`, and allows
    passing an offset to `start_drag`.
    
    sctk: Add support for drag-and-drop surface offsets
    
    Some applications using drag and drop want the top left corner of the
    drag surface (as happens without an offset). But others want the drag
    surface to be offset based on where the cursor is on the widget when
    starting the drag. This can just be `-1 * offset`, but may be scaled if
    the drag surface is a different size from the original widget.
    
    feat(sctk): support for overflow widget
    
    sctk: Fixes for cursor icon
    
    * With multiple windows, `SetCursor` is only sent for the focused
      window. Fixing a flicker between icons when two windows are using
      different cursors.
    * If there is a drag surface, let that surface set the cursor. And not
      any other.
    * Set cursor on `enter`, and when switching between CSDs and app area.
    
    Fixes https://github.com/pop-os/libcosmic/issues/533.
    
    improv(sctk): per-surface cursor position tracking
    
    feat(sctk): support ShowWindowMenu
    
    refactor(sctk): convert window actions
    
    sctk: Unmap subsurfaces instead of immediately destroying them
    
    Destroying a surface is immediate, rather than synchronized with
    commits.
    
    This fixes a flickering behavior with drag and drop in
    cosmic-workspaces.
    
    sctk: Add alpha setting to `Subsurface` widget
    
    sctk: Support `start_drag` with drags started from touch events

--- core/src/event/wayland/window.rs
+++ core/src/event/wayland/window.rs
@@ -1,12 +1,34 @@
 #![allow(missing_docs)]
 
-use sctk::reexports::csd_frame::{WindowManagerCapabilities, WindowState};
+use sctk::{
+    reexports::csd_frame::{WindowManagerCapabilities, WindowState},
+    shell::xdg::window::WindowConfigure,
+};
 
 /// window events
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone)]
 pub enum WindowEvent {
-    /// window manager capabilities
+    /// Window manager capabilities.
     WmCapabilities(WindowManagerCapabilities),
-    /// window state
+    /// Window state.
     State(WindowState),
+    /// Window configure event.
+    Configure(WindowConfigure),
+}
+
+impl PartialEq for WindowEvent {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::WmCapabilities(a), Self::WmCapabilities(b)) => a == b,
+            (Self::State(a), Self::State(b)) => a == b,
+            (Self::Configure(a), Self::Configure(b)) => {
+                a.capabilities == b.capabilities
+                    && a.state == b.state
+                    && a.decoration_mode == b.decoration_mode
+                    && a.new_size == b.new_size
+                    && a.suggested_bounds == b.suggested_bounds
+            }
+            _ => false,
+        }
+    }
 }
