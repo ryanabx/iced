79ab50a3d1835c87f103027af45207e207d61536
Adam Cosner <160804448+Adam-Cosner@users.noreply.github.com>
2025-02-20 04:34:39 -0800

    perf: use cached alpha mode instead of calling surface.get_capabilities every time the window is redrawn

--- wgpu/src/window/compositor.rs
+++ wgpu/src/window/compositor.rs
@@ -402,21 +402,6 @@ impl graphics::Compositor for Compositor {
         width: u32,
         height: u32,
     ) {
-        let caps = surface.get_capabilities(&self.adapter);
-        let alpha_mode = if caps
-            .alpha_modes
-            .contains(&wgpu::CompositeAlphaMode::PostMultiplied)
-        {
-            wgpu::CompositeAlphaMode::PostMultiplied
-        } else if caps
-            .alpha_modes
-            .contains(&wgpu::CompositeAlphaMode::PreMultiplied)
-        {
-            wgpu::CompositeAlphaMode::PreMultiplied
-        } else {
-            wgpu::CompositeAlphaMode::Auto
-        };
-
         surface.configure(
             &self.device,
             &wgpu::SurfaceConfiguration {
@@ -425,7 +410,7 @@ impl graphics::Compositor for Compositor {
                 present_mode: self.settings.present_mode,
                 width,
                 height,
-                alpha_mode,
+                alpha_mode: self.alpha_mode,
                 view_formats: vec![],
                 desired_maximum_frame_latency: 1,
             },
